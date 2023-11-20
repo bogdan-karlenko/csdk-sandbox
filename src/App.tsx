ea83177048c875569b954edd261cffccec01c9b0
Bogdan Karlenko <bogdan.karlenko@sisense.com>
2023-11-20 17:32:34 +0000

    Compose SDK Webinar

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import {
   CustomDrilldownResult,
   DataPoint,
   DrilldownWidget,
+  ExecuteQuery,
   SisenseContextProvider,
 } from '@sisense/sdk-ui';
+import { CustomContextMenu } from './custom-context-menu';
+import { CustomDrilldownBreadcrumbs } from './custom-drilldown-breadcrumbs';
 import * as DM from './data-models/sample-ecommerce';
+import { PlotlyBarChart } from './plotly-bar-chart';
 
 function App() {
   const { VITE_APP_SISENSE_URL, VITE_APP_SISENSE_TOKEN } = import.meta.env;
@@ -42,16 +46,40 @@ function App() {
     );
   };
 
-  // const plotlyBarChart = ({
-  //   drilldownFilters,
-  //   drilldownDimension,
-  //   onDataPointsSelected,
-  //   onContextMenu,
-  // }: CustomDrilldownResult) => (
-  // );
+  const plotlyBarChart = ({
+    drilldownFilters,
+    drilldownDimension,
+    onDataPointsSelected,
+    onContextMenu,
+  }: CustomDrilldownResult) => (
+    <ExecuteQuery
+      dataSource={DM.DataSource}
+      dimensions={[drilldownDimension]}
+      measures={[measures.sum(DM.Commerce.Revenue)]}
+      filters={drilldownFilters}
+    >
+      {(data) => (
+        <PlotlyBarChart
+          rawData={data}
+          onContextMenu={onContextMenu}
+          onDataPointsSelected={onDataPointsSelected}
+        />
+      )}
+    </ExecuteQuery>
+  );
 
   return (
     <SisenseContextProvider url={VITE_APP_SISENSE_URL} token={VITE_APP_SISENSE_TOKEN}>
+      <DrilldownWidget
+        initialDimension={DM.Commerce.AgeRange}
+        drilldownDimensions={[DM.Commerce.Gender, DM.Commerce.Condition]}
+        config={{
+          contextMenuComponent: CustomContextMenu,
+          breadcrumbsComponent: CustomDrilldownBreadcrumbs
+        }}
+      >
+        {plotlyBarChart}
+      </DrilldownWidget>
       <DrilldownWidget
         initialDimension={DM.Commerce.AgeRange}
         drilldownDimensions={[DM.Commerce.Gender, DM.Commerce.Condition]}
